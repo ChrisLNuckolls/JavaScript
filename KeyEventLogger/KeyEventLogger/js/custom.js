80b9393fe8e5d592efd0c81359be3fcfeb25480b
Christopher Nuckolls <chrislnuckolls@gmail.com>
2020-11-04 14:44:56 -0600

    Built key sequence tracker

---
+++ KeyEventLogger/KeyEventLogger/js/custom.js
@@ -0,0 +1,11 @@
+﻿var keysPressed = [];
+var eventText = 'zoey';
+
+window.addEventListener('keyup', (e) => {
+    keysPressed.push(e.key);
+    keysPressed.splice(-eventText.Length - 1, keysPressed.length - eventText.length);
+    if (keysPressed.join('').includes(eventText)) {
+        cornify_add();
+    }
+    console.log(keysPressed);
+});
